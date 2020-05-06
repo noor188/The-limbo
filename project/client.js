9de65eb5503167ff6fd9d867c882a1ece895bbf0
Amin Chamsaz <54604098+aminchamsaz@users.noreply.github.com>
2020-05-05 23:43:29 -0600

    Update client.js

--- project/client.js
+++ project/client.js
@@ -163,4 +163,18 @@ function gifts()
   }
 }
 
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("button");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
